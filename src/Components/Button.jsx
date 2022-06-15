2a49c71022557ea2d923f049ad9d464175a81ece
jang walia <jangiwalia83@gmail.com>
2022-06-15 11:41:38 -0700

    add stories for buttons

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Button.css'
-export default function Button({label,useClass,size}) {
-  
+import classNames from "classnames";
+export default function Button({useClass,label,handleClick}) {
+  const btnClass =  classNames("button",{
+    'base' : useClass === 'base',
+    'cancel' : useClass === 'cancel',
+    'disabled' : useClass === 'disabled'
+  })
   return (
-   <button className={useClass}>{label}</button>
-  )
+   <button onClick = {handleClick}  className={btnClass}>{label}</button>
+  ) 
 }
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,21 +1,35 @@
+import { action } from "@storybook/addon-actions";
 import Button from "../Components/Button";
 export default {
   title : "Button",
-  component: Button
+  component: Button,
+  argTypes : {handleClick : {action : "button Clicked"}}
+  
 }
 
-const Template = args => <Button {...args}/>
+const Template1 = args => <Button  {...args}/>
 
-export const Base = Template.bind({});
+
+export const Base = Template1.bind({});
 Base.args = {
   useClass : 'base',
   label : 'Add Course',
   size : 'md'
+ 
+ 
 }
 
-export const Cancel = Template.bind({});
+export const Cancel = Template1.bind({});
 Cancel.args = {
   useClass : 'cancel',
   label : 'Cancel',
   size : 'md'
 }
+
+export const Disabled = Template1.bind({});
+Disabled.args = {
+  useClass : 'disabled',
+  label : 'No Text',
+  size : 'md'
+}
+
